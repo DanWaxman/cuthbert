6b03a043bfcb55d0937b2bf0b45291980fdb2817
Adrien Corenflos <adrien.corenflos@gmail.com>
2024-11-06 18:23:14 +0000

    Add resampling atoms to the library. (#4)
    * Add resampling atoms
    
    * Reinstate the platform switch and tests
    
    * rename common to utils
    
    * Add pre-commit + tests workflow (#2)
    
    * Add tests workflow
    
    * Add tests
    
    * Skip pyright on tests folder
    
    ---------
    
    Co-authored-by: Adrien Corenflos <adrien.corenflos@gmail.com>
    
    * Add resampling atoms
    
    * Reinstate the platform switch and tests
    
    * rename common to utils
    
    * Update dependencies and run pre-commit.
    Make pytest pick the number of cores automatically.
    
    * Pre-commit
    
    * Make sure automated tests are super verbose.
    
    * Protocol types
    
    * Speed up code and tests
    
    * Simplify the conditional resampling test: we know what the marginal of a uniform index is: it's Cat(W), so no need to use the resampling method, we can just sample from the multinomial once.
    
    * pre-commit
    
    * Require a stronger precision.
    
    * Clean Array type hints
    
    ---------
    
    Co-authored-by: SamDuffield <34280297+SamDuffield@users.noreply.github.com>
    Co-authored-by: Sam Duffield <s@mduffield.com>

---
+++ resampling/protocols.py
@@ -0,0 +1,68 @@
+from typing import Protocol, runtime_checkable
+
+import jax
+from jax import Array
+from jax.typing import ArrayLike
+
+
+@runtime_checkable
+class Resampling(Protocol):
+    """Protocol for resampling operations."""
+
+    def __call__(self, key: Array, logits: ArrayLike, n: int) -> Array: ...
+
+
+@runtime_checkable
+class ConditionalResampling(Protocol):
+    """Protocol for conditional resampling operations."""
+
+    def __call__(
+        self,
+        key: Array,
+        logits: ArrayLike,
+        n: int,
+        pivot_in: int,
+        pivot_out: int,
+    ) -> Array: ...
+
+
+def resampling_decorator(func: Resampling, name: str, desc: str = "") -> Resampling:
+    """Decorate Resampling function with unified docstring."""
+
+    doc = f"""
+    {name} resampling. {desc}
+
+    Args:
+        key: PRNGKey to use in resampling
+        logits: Log-weights, possibly unnormalized.
+        n: Number of indices to sample.
+    
+    Returns:
+        Array of size n with indices to use for resampling.
+    """
+
+    func.__doc__ = doc
+    return jax.jit(func, static_argnames=("n",))
+
+
+def conditional_resampling_decorator(
+    func: ConditionalResampling, name: str, desc: str = ""
+) -> ConditionalResampling:
+    """Decorate ConditionalResampling function with unified docstring."""
+
+    doc = f"""
+    {name} conditional resampling. {desc}
+
+    Args:
+        key: PRNGKey to use in resampling
+        logits: Log-weights, possibly unnormalized.
+        n: Number of indices to sample
+        pivot_in: Index of the particle to keep
+        pivot_out: Value of the output at index `pivot_in`
+    
+    Returns:
+        Array of size n with indices to use for resampling.
+    """
+
+    func.__doc__ = doc
+    return jax.jit(func, static_argnames=("n",))

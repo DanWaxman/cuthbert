f7345d319e99797524ad34eb6026f3caeb187e63
Dan Waxman <dan.waxman1@gmail.com>
2026-02-26 13:58:07 -0500

    Introduce a Stop Gradient Resampling Decorator
    This commit introduces the stop-gradient resampler of [Scibior and Wood (2021)](https://arxiv.org/abs/2106.10314) as a resample decorator. Wrapping a base resampling method in the stop_gradient_decorator results in automatic differentation estimates of the score matching the classical estimates obtained via Fisher's identity.

--- cuthbertlib/resampling/__init__.py
+++ cuthbertlib/resampling/__init__.py
@@ -1,4 +1,10 @@
-from cuthbertlib.resampling import adaptive, killing, multinomial, systematic
+from cuthbertlib.resampling import (
+    adaptive,
+    killing,
+    multinomial,
+    stop_gradient,
+    systematic,
+)
 from cuthbertlib.resampling.adaptive import ess_decorator
 from cuthbertlib.resampling.protocols import ConditionalResampling, Resampling
 from cuthbertlib.resampling.utils import inverse_cdf
